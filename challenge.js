80d3fe6663a66e7028e64aa0b4278cf159917e28
AbdifataH-hersi <mohamedabdifatah107@gmail.com>
2024-09-12 12:11:58 +0900

    exercise done

--- challenge.js
+++ challenge.js
@@ -1,2 +1,18 @@
 // Good Luck! You got this 💪🏾
 // Write your code here.
+
+
+ 
+let  x= 10;
+let  y= 30;
+
+ if (x > 10){
+    console.log('the smallest out of two numbers is 10 ');
+ } else {
+    console.log('the largest out of two numbers is 30');
+ 
+ }
+
+
+
+   
